1096fb3119780076a51f2b7225332ac90cce96ca
Erik Faye-Lund <kusmabite@gmail.com>
2011-01-13 16:59:16 +0100

    example_bass: use folders for external deps
    BASS and SDL both add a set of headers and libs that needs
    to be included into the project. Allow the user to put these
    headers in a subfolder called "include" and the libs in a
    subfolder called "lib".

--- example_bass/example_bass.cpp
+++ example_bass/example_bass.cpp
@@ -10,12 +10,12 @@
 #include <SDL.h>
 #undef main /* avoid SDL's nasty SDLmain hack */
 #include <SDL_opengl.h>
+#include <bass.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <math.h>
 
 #include "../sync/sync.h"
-#include "bass.h"
 
 static const float bpm = 150.0f; /* beats per minute */
 static const int rpb = 8; /* rows per beat */
