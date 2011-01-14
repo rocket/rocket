23c512ac7094befa98b413444b12fef56d737c56
Erik Faye-Lund <kusmabite@gmail.com>
2011-01-14 12:22:19 +0100

    edit-test

--- qt-editor/trackview.cpp
+++ qt-editor/trackview.cpp
@@ -20,17 +20,57 @@ int TrackModel::rowCount(const QModelIndex & /* parent */) const
 
 int TrackModel::columnCount(const QModelIndex & /* parent */) const
 {
-	return 3;
+	return 32;
+}
+
+Qt::ItemFlags TrackModel::flags(const QModelIndex &index) const
+{
+	if (!index.isValid())
+		return 0;
+
+	return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
 }
 
 QVariant TrackModel::data(const QModelIndex &index, int role) const
 {
-	if (!index.isValid() || role != Qt::DisplayRole)
+	if (!index.isValid())
 		return QVariant();
-	if (index.row() % 4 != 0)
+
+	QString str;
+	switch (role) {
+	case Qt::DisplayRole:
+		if (index.row() % 4 != 0)
+			return QVariant();
+		return str.setNum(float(index.row()) / 16);
+
+	case Qt::BackgroundRole:
+		if (index.row() % 4 == 0 && index.column() % 2 == 0)
+			return QBrush(QColor(255, 0, 0, 32));
+		if (index.row() % 8 == 0)
+			return QBrush(QColor(0, 0, 0, 16));
 		return QVariant();
-	return QVariant(float(index.row()) / 16);
+
+	case Qt::EditRole:
+		if (index.row() % 4 != 0)
+			return QVariant();
+		return str.setNum(float(index.row()) / 16);
+	}
+
+	return QVariant();
 }
+
+bool TrackModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+	bool ok;
+	value.toFloat(&ok);
+
+	if (!ok)
+		return false;
+
+	dataChanged(index, index);
+	return true;
+}
+
 
 QVariant TrackModel::headerData(int section,
 	Qt::Orientation orientation,
--- qt-editor/trackview.h
+++ qt-editor/trackview.h
@@ -15,6 +15,9 @@ public:
 
 	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+	Qt::ItemFlags flags(const QModelIndex &index) const;
+
+	bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
 };
 
 class TrackView : public QTableView {
