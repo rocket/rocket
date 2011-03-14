3f7a4a7fca2b89ea2b638b442ea6c5cf990b6790
Erik Faye-Lund <kusmabite@gmail.com>
2011-03-14 23:26:19 +0100

    dotrocket: add example
    Create a rudimentary C# example using DX11 for rendering and XAudio2
    for sound (through SlimDX). The player is hacky due to what seems to
    be issues with either the implementation or documentation of XAudio2
    and/or SlimDX' intergration of it.

---
+++ contrib/DotRocket/example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotRocket Example")]
+[assembly: AssemblyDescription("GNU Rocket Example for DotNet")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GNU Rocket Foundation")]
+[assembly: AssemblyProduct("GNU Rocket")]
+[assembly: AssemblyCopyright("Copyright © GNU Rocket Foundation 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e7bf32b-89ee-4e48-91ee-9008f83c7947")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
