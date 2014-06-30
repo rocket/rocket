9d151efe0e2df1718f0763dd01897fa77ddb6078
Erik Faye-Lund <erik@hue.no>
2014-06-30 17:20:13 +0200

    do not guard against empty pkg.homepage
    We already know that it's not empty, so there's no point in
    even checking.

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         meta  :{
             bannerLite:'/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
                 '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+                '* <%= pkg.homepage %>\n' +
                 '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
                 ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>*/\n'
         },
